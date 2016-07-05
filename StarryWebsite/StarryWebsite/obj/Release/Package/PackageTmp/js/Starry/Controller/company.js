523d21c9da7b1a0e4d3b571fac06b6fc613b44ea
jason <jason@DESKTOP-GBVBPT3>
2016-07-04 21:06:36 -0700

    Clean up Starry.Website staged items

--- StarryWebsite/StarryWebsite/obj/Release/Package/PackageTmp/js/Starry/Controller/company.js
+++ StarryWebsite/StarryWebsite/obj/Release/Package/PackageTmp/js/Starry/Controller/company.js
@@ -1,18 +1,54 @@
 //// <reference path="../../scripts/typings/_all.d.ts" />
+var App = angular.module("Starry", ["ngRoute"]);
 var Starry;
 (function (Starry) {
     var CompanyController = (function () {
         function CompanyController(scope, $http, $q) {
             this.scope = scope;
-            //var userId = $routeParams.code;
             this.scope.companyScope = "dashboard";
         }
         CompanyController.$inject = ['$scope', '$http', '$q'];
         return CompanyController;
     }());
     Starry.CompanyController = CompanyController;
-    angular
-        .module("Starry", [])
-        .controller("CompanyController", CompanyController);
+    App.controller("CompanyController", CompanyController)
+        .config(['$routeProvider',
+        function ($routeProvider) {
+            $routeProvider
+                .when("/dashboard", {
+                templateUrl: "company-dashboard.html",
+                controller: "CompanySearchController as vm"
+            })
+                .when("/goal", {
+                templateUrl: "company-goal.html",
+                controller: "CompanySearchController as vm"
+            })
+                .when("/search", {
+                templateUrl: "company-search.html",
+                controller: "CompanySearchController as vm"
+            })
+                .when("/select", {
+                templateUrl: "company-select.html",
+                controller: "CompanySearchController as vm"
+            })
+                .when("/track", {
+                templateUrl: "company-track.html",
+                controller: "CompanySearchController as vm"
+            })
+                .when("/history", {
+                templateUrl: "company-history.html",
+                controller: "CompanySearchController as vm"
+            })
+                .when("/help", {
+                templateUrl: "company-help.html",
+                controller: "CompanySearchController as vm"
+            })
+                .when("/storeDetails/:storeId", {
+                templateUrl: "/templates/storeDetailView.html",
+                controller: "StoreDetailCtrl as vm"
+            })
+                .otherwise("");
+        }
+    ]);
 })(Starry || (Starry = {}));
 //# sourceMappingURL=company.js.map
