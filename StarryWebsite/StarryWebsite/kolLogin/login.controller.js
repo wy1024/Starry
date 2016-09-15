4d62cc77c45d20750e60688ba1725ac5e0f13445
wy1024 <wy1024@uw.edu>
2016-09-14 21:35:52 -0700

    add missing login.controller

---
+++ StarryWebsite/StarryWebsite/kolLogin/login.controller.js
@@ -0,0 +1,35 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('Starry')
+        .controller('KolLoginController', LoginController);
+
+    LoginController.$inject = ['$location', 'KolAuthenticationService', 'KolFlashService'];
+    function LoginController($location, AuthenticationService, FlashService) {
+        var vm = this;
+
+        vm.login = login;
+
+        (function initController() {
+            // reset login status
+            AuthenticationService.ClearCredentials();
+        })();
+
+        function login() {
+            vm.dataLoading = true;
+            AuthenticationService.Login(vm.username, vm.password, function (response) {
+                console.log(response);
+                if (response === 'true') {
+                    AuthenticationService.SetCredentials(vm.username, vm.password);
+                    $location.path('/');
+                } else {
+                    // false
+                    FlashService.Error('用户名或密码错误');
+                    vm.dataLoading = false;
+                }
+            });
+        };
+    }
+
+})();
